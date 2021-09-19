bd94c426192e305ac5a901c938bbd2e6673517fe
ZhengTian <38405540+tangzt96@users.noreply.github.com>
2021-09-19 14:54:49 +0800

    added components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
-import {Navbar, NavbarBrand} from 'reactstrap'
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
+import React, {Component, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Focus.io</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component { //lower letters tags are DOM tags
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  
+  render(){
+    return (
+      <div> 
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Focus.io</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes = {this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
