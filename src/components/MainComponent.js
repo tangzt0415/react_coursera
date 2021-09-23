561e6ae6cd054be7a223564d8e8cfe5b39db021d
ZhengTian <38405540+tangzt96@users.noreply.github.com>
2021-09-24 00:17:05 +0800

    week 2, router, switches

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,17 @@
 import Menu from './MenuComponent';
 import Home from './HomeComponent.js';
-import {DISHES} from '../shared/dishes';
-import React, {Component, useState} from 'react';
+import { DISHES } from '../shared/dishes'
+import React, {Component} from 'react';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import DishDetail from "./DishdetailComponenet";
+import About from './AboutComponent';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { COMMENTS } from '../shared/comments';
+
 import { Switch, Route, Redirect } from 'react-router';
+import DishDetail from './DishdetailComponent';
 
 
 
@@ -15,7 +21,9 @@ class Main extends Component { //lower letters tags are DOM tags
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders:LEADERS
     };
   }
 
@@ -23,15 +31,37 @@ class Main extends Component { //lower letters tags are DOM tags
 
     const HomePage = () =>{
       return(
-        <Home />
-      )
+        <Home dish={this.state.dishes.filter((dish)=> dish.featured)[0]} 
+          promotion={this.state.promotions.filter((promo)=> promo.featured)[0]}
+          leader={this.state.leaders.filter((leader)=> leader.featured)[0]}
+          />
+      );
+    };
+    const AboutUsPage = () =>{
+      return(
+        <About
+          leaders={this.state.leaders}/>
+      );
+    };
+
+    const DishWithId = ({match}) =>{
+      return(
+        <DishDetail 
+        dish={this.state.dishes.filter((dish)=>dish.id === parseInt(match.params.dishId,10))[0]}
+        comments={this.state.comments.filter((comment)=> comment.dishID === parseInt(match.params.dishId,10))}
+        />
+      );
     }
+
     return (
       <div> 
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={AboutUsPage}/>
           <Redirect to="/home" />
         </Switch>
         <Footer />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { CardText,CardBody,CardTitle,Card,CardImg,CardImgOverlay } from 'reactstrap';
+import { Breadcrumb,BreadcrumbItem,CardTitle,Card,CardImg,CardImgOverlay } from 'reactstrap';
+import { Link } from "react-router-dom";
+
 
     function RenderMenuItem({dish, onClick}){
         return(
-            <Card onClick={()=> onClick(dish.id)}>                   
+            <Card>
+                <Link to={`/menu/${dish.id}`} >                   
                 <CardImg src={dish.image} alt={dish.name} />
                 <CardImgOverlay className="ml-5">
                     <CardTitle heading>{dish.name}</CardTitle>
                 </CardImgOverlay>   
+                </Link>
             </Card>
         );
     }
@@ -16,13 +20,23 @@ import { CardText,CardBody,CardTitle,Card,CardImg,CardImgOverlay } from 'reactst
         const menu = props.dishes.map((dish) => {
             return(
                 <div key={dish.id} className="col-12 col-md-5"> 
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                    <RenderMenuItem dish={dish}/>
                 </div>
             );
         }); 
 
         return (
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row">
                         {menu}
                 </div>
